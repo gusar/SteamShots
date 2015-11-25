e55c3cee1d690ca253931e852b88c8929562d51a
andy <andy.lahs@gmail.com>
2015-11-25 18:33:10 +0000

    Add recycler view adapter, swipe refresh

--- app/src/main/java/com/andylahs/steamshots/view/UserScreenshotsActivity.java
+++ app/src/main/java/com/andylahs/steamshots/view/UserScreenshotsActivity.java
@@ -2,10 +2,12 @@ package com.andylahs.steamshots.view;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -26,6 +28,8 @@ public class UserScreenshotsActivity extends BaseActivity implements HttpReturnL
   private static final String LOG_TAG = UserScreenshotsActivity.class.getSimpleName();
   private RecyclerView recyclerView;
   private ScreenshotRecyclerViewAdapter recyclerViewAdapter;
+  private SwipeRefreshLayout swipeRefreshLayout;
+  private int screenHeight;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -38,26 +42,30 @@ public class UserScreenshotsActivity extends BaseActivity implements HttpReturnL
     WindowManager wm = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
     wm.getDefaultDisplay().getMetrics(displayMetrics);
 //    int screenWidth = displayMetrics.widthPixels;
-    int screenHeight = displayMetrics.heightPixels;
+    screenHeight = displayMetrics.heightPixels;
 
     /*
     *
     * */
 
+    swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.activity_swipe_refresh_layout);
     recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
     recyclerView.addItemDecoration(new MarginDecoration(this));
-//    recyclerView.setHasFixedSize(true);
     recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-    recyclerViewAdapter = new ScreenshotRecyclerViewAdapter(
-        UserScreenshotsActivity.this,
-        new ArrayList<Screenshot>(),
-        screenHeight
-    );
-    recyclerView.setAdapter(recyclerViewAdapter);
-
-    ScrListAsyncTask scrListAsyncTask = new ScrListAsyncTask();
-    scrListAsyncTask.setOnHttpReturnListener(this);
-    scrListAsyncTask.execute();
+    initAdapter(screenHeight);
+    swipeRefreshLayout.setColorSchemeResources(R.color.blue, R.color.green, R.color.blue_grey);
+    swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+      @Override
+      public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+          @Override
+          public void run() {
+            initAdapter(screenHeight);
+            swipeRefreshLayout.setRefreshing((false));
+          }
+        }, 1500);
+      }
+    });
 
     /*
     *
@@ -80,6 +88,18 @@ public class UserScreenshotsActivity extends BaseActivity implements HttpReturnL
     NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
     navigationView.setNavigationItemSelectedListener(this);
 
+  }
+
+  private void initAdapter(int screenHeight) {
+    recyclerViewAdapter = new ScreenshotRecyclerViewAdapter(
+        UserScreenshotsActivity.this,
+        new ArrayList<Screenshot>(),
+        screenHeight
+    );
+    recyclerView.setAdapter(recyclerViewAdapter);
+    ScrListAsyncTask scrListAsyncTask = new ScrListAsyncTask();
+    scrListAsyncTask.setOnHttpReturnListener(this);
+    scrListAsyncTask.execute();
   }
 
 //  @Override
